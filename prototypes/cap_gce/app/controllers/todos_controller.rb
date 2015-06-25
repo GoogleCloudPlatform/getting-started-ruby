1609c78c605d015a3bad36fed4679ebb0a59209f
Jeff Mendoza <jeffmendoza@google.com>
2015-06-25 15:06:26 -0700

    Add resqueue to cap_gce example.
    Change-Id: I3badfae1d6807488d198fc7220dd59da691a3739

--- prototypes/cap_gce/app/controllers/todos_controller.rb
+++ prototypes/cap_gce/app/controllers/todos_controller.rb
@@ -28,6 +28,7 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
+        Resque.enqueue(Finisher, @todo.id)
         format.html { redirect_to @todo, notice: 'Todo was successfully created.' }
         format.json { render :show, status: :created, location: @todo }
       else
