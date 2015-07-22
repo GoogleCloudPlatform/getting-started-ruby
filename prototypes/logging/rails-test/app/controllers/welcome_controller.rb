e1a35c2bcb5ffeb60191d1c048613665c6a93a72
remi Taylor <remi@remitaylor.com>
2015-07-22 14:19:46 -0700

    Move Bookshelf application to root

--- prototypes/logging/rails-test/app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-
-  def index
-    Rails.logger.info "YAY!  You logged something!  :)"
-
-    render text: "Hello from Rails @ #{Time.now}"
-  end
-
-end
