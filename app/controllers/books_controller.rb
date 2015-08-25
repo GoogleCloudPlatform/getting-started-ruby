dc31f57eebaa99822758fed74171d9c7feadb7e0
remi Taylor <remi@remitaylor.com>
2015-08-25 12:56:44 -0700

    Change default structured data to use ActiveRecord

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,22 +17,38 @@ class BooksController < ApplicationController
   PER_PAGE = 10
 
   def index
-    @books, @cursor = Book.query limit: PER_PAGE, cursor: params[:cursor]
+    page = params[:page] ? params[:page].to_i : 0
+
+    @books = Book.limit(PER_PAGE).offset(PER_PAGE * page)
+    @next_page = page + 1 if @books.count == PER_PAGE
   end
 # [END index]
 
+  # [START new_and_edit]
   def new
     @book = Book.new
-  end
-
-  def show
-    @book = Book.find params[:id]
   end
 
   def edit
     @book = Book.find params[:id]
   end
+  # [END new_and_edit]
 
+  # [START show]
+  def show
+    @book = Book.find params[:id]
+  end
+  # [END show]
+
+  # [START destroy]
+  def destroy
+    @book = Book.find params[:id]
+    @book.destroy
+    redirect_to books_path
+  end
+  # [END destroy]
+
+  # [START update]
   def update
     @book = Book.find params[:id]
 
@@ -43,15 +59,9 @@ class BooksController < ApplicationController
       render :edit
     end
   end
+  # [END update]
 
-  def destroy
-    @book = Book.find params[:id]
-    @book.destroy
-    redirect_to books_path
-  end
-
-  before_filter :convert_published_on_to_date
-
+  # [START create]
   def create
     @book = Book.new book_params
 
@@ -69,11 +79,6 @@ class BooksController < ApplicationController
     params.require(:book).permit :title, :author, :published_on, :description,
                                  :cover_image
   end
-
-  def convert_published_on_to_date
-    if params[:book] && params[:book][:published_on].present?
-      params[:book][:published_on] = Time.parse params[:book][:published_on]
-    end
-  end
+  # [END create]
 
 end
