2a46019d79185d3946cb5eff273e0ea8b8220b65
remi Taylor <remi@remitaylor.com>
2015-09-03 14:29:22 -0700

    Region tag for Book class

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,6 +11,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START book]
 class Book < ActiveRecord::Base
   validates :title, presence: true
 end
+# [END book]
