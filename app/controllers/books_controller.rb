b8da6f2f5e742071d99d59650ddb8e1f6a725416
remi Taylor <remi@remitaylor.com>
2015-09-03 10:07:03 -0700

    Update pagination so second page displays ?page=2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,10 +17,10 @@ class BooksController < ApplicationController
   PER_PAGE = 10
 
   def index
-    page = params[:page] ? params[:page].to_i : 0
-
-    @books = Book.limit(PER_PAGE).offset(PER_PAGE * page)
-    @next_page = page + 1 if @books.count == PER_PAGE
+    page_number = params[:page] ? params[:page].to_i : 1
+    book_offset = PER_PAGE * (page_number - 1)
+    @books      = Book.limit(PER_PAGE).offset(book_offset)
+    @next_page  = page_number + 1 if @books.count == PER_PAGE
   end
 # [END index]
 
