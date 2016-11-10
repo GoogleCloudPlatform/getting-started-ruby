6deef160b636b4a20568ee65abc921c5622d0f80
Frank Natividad <franknatividad@google.com>
2016-11-10 11:28:10 -0800

    Updating 4-auth-datastore environment and google-cloud gem
    * Updated app.yaml env: flex and small clean up
    * Update gcloud usage to google-cloud usage
    * Removed deprecated code
    * Updated Datastore sample of 4-auth
    * Updated Datastore rspec of 4-auth

--- 4-auth/config/application.rb
+++ 4-auth/config/application.rb
@@ -17,7 +17,6 @@ require "rails"
 
 # Pick the frameworks you want:
 require "active_model/railtie"
-require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
@@ -29,8 +28,5 @@ Bundler.require(*Rails.groups)
 module Bookshelf
   class Application < Rails::Application
     config.x.settings = Rails.application.config_for :settings
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
