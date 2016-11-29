a3b929e763a6bfc02b8e05843f7265769927492f
Frank Natividad <franknatividad@google.com>
2016-11-29 15:47:24 -0800

    Updating book lookup in step 7 to reflect step 6

--- 7-compute-engine/app/jobs/lookup_book_details_job.rb
+++ 7-compute-engine/app/jobs/lookup_book_details_job.rb
@@ -11,6 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# [START lookup_books]
 require "google/apis/books_v1"
 
 BooksAPI = Google::Apis::BooksV1
@@ -19,22 +20,32 @@ class LookupBookDetailsJob < ActiveJob::Base
   queue_as :default
 
   def perform book
-    Rails.logger.info "Lookup details for book #{book.id} #{book.title.inspect}"
+    Rails.logger.info "[BookService] Lookup details for book" +
+                      "#{book.id} #{book.title.inspect}"
 
     # Create Book API Client
     book_service = BooksAPI::BooksService.new
-    book_service.authorization = nil # Books API does not require authentication
+    # Books API does not require authentication
+    book_service.authorization = nil
 
     # Lookup a list of relevant books based on the provided book title.
     book_service.list_volumes book.title, order_by: "relevance" do |results, error|
+      # Error ocurred soft-failure
       if error
-        Rails.logger.error "[BookService] " + error
-        raise "BookService list_volumes ERROR!"
+        Rails.logger.error "[BookService] #{error.inspect}"
+        break
+      end
+
+      # Book was not found
+      if results.total_items.zero?
+        Rails.logger.info "[BookService] #{book.title} was not found."
+        break
       end
 
       # List of relevant books
       volumes = results.items
-
+# [END lookup_books]
+      # [START choose_volume]
       # To provide the best results, find the first returned book that
       # includes title and author information as well as a book cover image.
       best_match = volumes.find {|volume|
@@ -43,7 +54,9 @@ class LookupBookDetailsJob < ActiveJob::Base
       }
 
       volume = best_match || volumes.first
+      # [END choose_volume]
 
+      # [START update_book]
       if volume
         info   = volume.volume_info
         images = info.image_links
@@ -53,13 +66,16 @@ class LookupBookDetailsJob < ActiveJob::Base
         publication_date = Date.parse publication_date
 
         book.author       = info.authors.join(", ") unless book.author.present?
-        book.published_on = publication_date     unless book.published_on.present?
-        book.description  = info.description        unless book.description.present?
-        book.image_url    = images.try(:thumbnail)  unless book.image_url.present?
+        book.published_on = publication_date unless book.published_on.present?
+        book.description  = info.description unless book.description.present?
+        book.image_url    = images.try(:thumbnail) unless book.image_url.
+                                                               present?
         book.save
       end
+      # [END update_book]
 
-      Rails.logger.info "(#{book.id}) Complete"
+      Rails.logger.info "[BookService] (#{book.id}) Complete"
     end
   end
 end
+# [END book_lookup]
