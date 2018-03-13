3dc2d3e1d0c8fdd200811f629a549b42c03d32d5
Daniel Azuma <dazuma@google.com>
2018-03-13 14:05:48 -0400

    Update copyright notice on new files

--- optional-kubernetes-engine/spec/storage_book_extensions.rb
+++ optional-kubernetes-engine/spec/storage_book_extensions.rb
@@ -1,4 +1,5 @@
-# Copyright 2015, Google, Inc.
+# Copyright 2018 Google LLC
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -10,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 
 # Additional methods added to the Book class for testing only.
 # Implements a minimal in-memory storage service.
