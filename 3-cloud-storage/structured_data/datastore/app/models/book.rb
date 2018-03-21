1d819731918b873ab98bc1a0d975cb978769f751
Frank Natividad <franknatividad@google.com>
2018-03-20 17:26:06 -0700

    Use project_id: instead of project:

--- 3-cloud-storage/structured_data/datastore/app/models/book.rb
+++ 3-cloud-storage/structured_data/datastore/app/models/book.rb
@@ -37,8 +37,8 @@ class Book
   # The dataset is used to create, read, update, and delete entity objects.
   def self.dataset
     @dataset ||= Google::Cloud::Datastore.new(
-      project: Rails.application.config.
-                     database_configuration[Rails.env]["dataset_id"]
+      project_id: Rails.application.config.
+                        database_configuration[Rails.env]["dataset_id"]
     )
   end
 
